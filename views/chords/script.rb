f198b678705f3fb58c967bf5ab170585d8856a88
Pascal Rainville <pascal.rainville@gmail.com>
2022-11-21 16:27:47 -0500

    .

--- views/chords/script.rb
+++ views/chords/script.rb
@@ -1,7 +1,5 @@
-Dir.glob("./*").each do |f|
-  #data = File.open(f, 'r').read
-  puts f
-  data = File.read(f)
-  data = data.split('</pre>')[0]
-  File.write(f, data)
+')
+  if s.length > 1
+    File.write(f, s[1])
+  end
 end

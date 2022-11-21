d9b9ad6a8b04706836034f5bd656061b8b8d8fe5
Pascal Rainville <pascal.rainville@gmail.com>
2022-11-21 17:29:18 -0500

    .

---
+++ build.js
@@ -0,0 +1,21 @@
+const { exec } = require("child_process");
+
+let songs = fs.readdirSync(path.join(__dirname, 'views', 'chords'));
+
+let pages = [
+  {url: '/', out: '/docs/index.html'},
+]
+
+songs.forEach(song => {
+  let name = encodeURI(song)ncodeURI(song)
+  pages.push({url: '/c/'+name, out: '/docs/'+name+'.html'})
+})
+
+  //full = File.join(OUT_DIR, _relative_path(path))
+  //FileUtils.mkdir_p(full) unless File.directory?(full)
+  //system("wget #{_fullpath(path)} -q -O #{full}/index.html") # -q => quiet; -O => output file name; -k => relative file path
+
+pages.forEach(page => {
+  // -q => quiet; -O => output file name; -k => relative file path
+  exec("wget http://localhost:3000"+page.url+" -q -O "+page.out)
+})

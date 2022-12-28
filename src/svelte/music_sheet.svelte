a0813ab4497b90acf21d5ac09653c0b27d88ba16
Pascal Rainville <pascal.rainville@gmail.com>
2022-12-28 13:19:48 -0500

    .

--- src/svelte/music_sheet.svelte
+++ src/svelte/music_sheet.svelte
@@ -11,7 +11,6 @@ onMount(() => {
   const div = document.getElementById('music-sheet-2');
   const renderer = new Renderer(div, Renderer.Backends.SVG);
   
-  // Configure the rendering context.
   renderer.resize(500, 500);
   const context = renderer.getContext();
   context.setFont('Arial', 10);
@@ -19,13 +18,9 @@ onMount(() => {
   context.setStrokeStyle(color);
   context.setFillStyle(color);
   
-  // Create a stave of width 400 at position 10, 40.
   const stave = new Stave(10, 40, 400);
-  
-  // Add a clef and time signature.
   stave.addClef('treble').addTimeSignature('4/4');
   
-  // Connect it to the rendering context and draw!
   stave.setContext(context).draw();
 })
 </script>
